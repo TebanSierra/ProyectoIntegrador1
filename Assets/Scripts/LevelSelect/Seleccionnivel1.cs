57dcdc94d97d63461f5b6605ea848ba451cb8d9f
amboraes <amboraes@hotmail.com>
2017-05-10 18:30:38 -0500

    Actualizacion seleccion de niveles
    Se hace la seleccion de niveles para el juego de laberinto, conjunto con
    la explicacion de como se debe proceder dentro del juego

---
+++ Assets/Scripts/LevelSelect/Seleccionnivel1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Seleccionnivel1 : MonoBehaviour {
+    public Image imagen;
+    private bool activo;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+     public void nivel(string a)
+    {
+        StartCoroutine(DoTheDance(a));
+    }
+    public IEnumerator DoTheDance(string a)
+    {
+        imagen.gameObject.SetActive(true);
+        yield return new WaitForSeconds(15f);
+        imagen.gameObject.SetActive(false);
+        SceneManager.LoadScene(a);
+
+    }
+}
