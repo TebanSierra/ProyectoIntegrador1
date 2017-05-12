35ad226ed3f80beef27164cf96341de9dabb36d6
amboraes <amboraes@hotmail.com>
2017-05-11 21:26:17 -0500

    Algunas modificaciones
    Se agregan las descripciones de nonogramas y codificadion, y se corrigen
    detalles menores

---
+++ Assets/Scripts/MainMenu/ControlSeleclvl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ControlSeleclvl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public Image imagen;
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(Lvlsel(sceneName));
+        
+
+    }
+    public IEnumerator Lvlsel(string sceneName)
+    {
+
+        imagen.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        imagen.gameObject.SetActive(false);
+        SceneManager.LoadScene(sceneName);
+
+    }
+}
